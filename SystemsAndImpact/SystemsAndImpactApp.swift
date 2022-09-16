73ccb53071d787316761205cb7e20c39b0db0b0c
darynamm <darynamanuilenko@gmail.com>
2022-09-16 08:17:03 -0600

    created repo and made ignored files

---
+++ SystemsAndImpact/SystemsAndImpactApp.swift
@@ -0,0 +1,17 @@
+//
+//  SystemsAndImpactApp.swift
+//  SystemsAndImpact
+//
+//  Created by Manuilenko, Daryna on 9/16/22.
+//
+
+import SwiftUI
+
+@main
+struct SystemsAndImpactApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
