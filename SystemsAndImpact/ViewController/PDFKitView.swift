a4e6353b258111185df697ebbcab9f2e5b4d7366
darynamm <darynamanuilenko@gmail.com>
2022-09-22 07:53:50 -0600

    made a new view that will help process the pdf

---
+++ SystemsAndImpact/ViewController/PDFKitView.swift
@@ -0,0 +1,24 @@
+//
+//  PDFKitView.swift
+//  SystemsAndImpact
+//
+//  Created by Manuilenko, Daryna on 9/22/22.
+//
+
+import SwiftUI
+
+struct PDFKitView: View
+{
+    var body: some View
+    {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+struct PDFKitView_Previews: PreviewProvider
+{
+    static var previews: some View
+    {
+        PDFKitView()
+    }
+}
